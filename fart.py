78169544977ef9a27ed61f6c007f72a02528863f
Otto Paulsen <otto.paulsen@itema.no>
2015-04-21 23:16:39 +0200

    Tja...

---
+++ fart.py
@@ -0,0 +1,127 @@
+
+import RPi.GPIO as GPIO
+import time
+import picamera
+import sys
+import select
+import io
+
+from op_keyboard import read_single_keypress
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+redLed = 4 # pin 7
+greenLed = 3 # pin 5
+
+startButton = 2 # pin 3 
+stopButton = 10 # pin 19
+
+# LEDs
+GPIO.setup(greenLed, GPIO.OUT)
+GPIO.setup(redLed, GPIO.OUT)
+
+#Buttons
+GPIO.setup(startButton, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(stopButton, GPIO.IN, GPIO.PUD_UP)
+
+camera = picamera.PiCamera()
+#camera.brightness = 60
+camera.hflip = True
+camera.vflip = True
+
+GPIO.output(greenLed, 1)
+time.sleep(1)
+GPIO.output(greenLed, 0)
+GPIO.output(redLed, 1)
+time.sleep(1)
+GPIO.output(redLed, 0)
+
+timing = False
+
+videoCounter = 0
+
+
+
+def write_video1(stream):
+    print('Writing video!')
+    with stream.lock:
+        # Find the first header frame in the video
+        for frame in stream.frames:
+            if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                stream.seek(frame.position)
+                break
+        # Write the rest of the stream to disk
+        with io.open('video.h264', 'wb') as output:
+            output.write(stream.read())
+
+
+def write_video2(stream):
+    # Write the entire content of the circular buffer to disk. No need to
+    # lock the stream here as we're definitely not writing to it
+    # simultaneously
+    print('Writing video before!')
+    with io.open('before.h264', 'wb') as output:
+        for frame in stream.frames:
+            if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                stream.seek(frame.position)
+                break
+        while True:
+            buf = stream.read1()
+            if not buf:
+                break
+            output.write(buf)
+    # Wipe the circular stream once we're done
+    stream.seek(0)
+    stream.truncate()
+
+
+stream = picamera.PiCameraCircularIO(camera, seconds=5)
+camera.annotate_text = 'Waiting'
+camera.start_recording(stream, format='h264')
+print ("Recording started")
+camera.wait_recording(1)
+
+c = ''
+
+print("Starting loop. Press t to terminate, i to start, o to stop.")
+
+while c != 't':
+
+    c = read_single_keypress()
+
+    if GPIO.input(startButton) == False or c == 'i' :
+    	if not timing:
+    	    startTime = time.time()
+    	    timing = True
+            print("Start: " + str(startTime))
+            camera.annotate_text = 'Timer started'
+            write_video2(stream)
+            print("Splitting video")
+            camera.split_recording('after.h264')
+
+            videoCounter += 1
+
+    if timing:
+        pass
+        #camera.annotate_text = str(startTime - time.time())
+
+    if GPIO.input(stopButton) == False or c == 'o':
+    	if timing:
+    	    stopTime = time.time()
+    	    timing = False
+    	    runTime = stopTime - startTime
+    	    print("Stop:  " + str(stopTime))
+    	    print("Run time: " + str(runTime))
+            camera.annotate_text = 'Timer stopped'
+            camera.wait_recording(5)
+            camera.stop_recording()
+            print("Stopped recording")
+
+    GPIO.output(greenLed, GPIO.input(startButton))
+    GPIO.output(redLed, GPIO.input(stopButton))
+
+
+
+
