6a2c44b9304b8358d8e1f840221af790a55ad1e7
Otto Paulsen <otto.paulsen@itema.no>
2015-04-29 21:57:51 +0200

    Got video working

--- vid.py
+++ vid.py
@@ -6,6 +6,7 @@ camera = picamera.PiCamera(framerate = 25)
 camera.hflip = True
 camera.vflip = True
 stream = picamera.PiCameraCircularIO(camera, seconds=5)
+camera.annotate_text = "Buffering video"
 camera.start_recording(stream, format='h264')
 print ("Camera started, recording to buffer")
 camera.wait_recording(1)
@@ -16,24 +17,27 @@ stopTime = startTime
 runTime = startTime
 videoCounter = 0
 filename = "video-0.h264"
+
+
 print ("Ready (time:" + str(startTime) + ")!")
 
 def write_buf_to_file(stream, filename):
-    with stream.lock:
-	    # Find the first header frame in the video
+	global output
+	with stream.lock:
+    	# Find the first header frame in the video
 	    for frame in stream.frames:
 	        if frame.frame_type == picamera.PiVideoFrameType.sps_header:
 	            stream.seek(frame.position)
 	            break
 	    # Write the rest of the stream to disk
-	    with io.open(filename, 'wb') as output:
-	        output.write(stream.read())
-		return output
+	    output = io.open(filename, 'wb')
+        output.write(stream.read())
+	return output
 	
 
 @route('/start')
 def start():
-	global camera, stream, stopped, timing, startTime, videoCounter, filename
+	global camera, stream, stopped, timing, startTime, videoCounter, filename, output
 	res = "Already started. Cannot start again until stopped."
 	if not timing:
 		startTime = time.time()
@@ -43,13 +47,14 @@ def start():
 		filename = "video-" + str(videoCounter) + ".h264"
 		print("Start: " + str(startTime))
 		camera.annotate_text = 'Timer started'
-		camera.split_recording(write_buf_to_file(stream, filename))
+		output = write_buf_to_file(stream, filename)
+		camera.split_recording(output)
 		res = "Started at " + str(startTime)
 	return res
 
 @route('/stop')
 def stop():
-	global camera, stream, stopped, timing, stopTime, runTime, videoCounter
+	global camera, stream, stopped, timing, stopTime, runTime, videoCounter, output
 	res = "Not started. Cannot stop."
 	if timing and not stopped:
 		stopTime = time.time()
@@ -61,6 +66,7 @@ def stop():
 		print("Camera running 5 more seconds")
 		camera.wait_recording(5)
 		camera.stop_recording()
+		output.close()
 		res = "Stopped at " + str(stopTime) + ", run time " + str(runTime)
 	return res
 
